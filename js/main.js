12e307abdb987b58e62b463d8d4796a604c89367
RodriguezSanRosales <san.rodzrosales@gmail.com>
2016-10-25 21:28:21 +0000

    agregando funciones

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
-var agrega = document.getElementById("agregaLista");
+window.addEventListener("load",function(){
+	var agregaLista = document.getElementById("agregaLista");
+	var formList = document.getElementById("formList");
+	
+	//agrega un focus con color de fondo
+	
 
-agrega.addEventListener("focus", function(){
-	agrega.style.background = "#F693FF";
+
+	agregaLista.addEventListener("click",function(){
+		//var contenedor = document.createElement("div"); //crea un elemento div
+
+		var listaInpu = document.createElement("input"); // crea un elemento input
+		listaInpu.setAttribute("placeholder", "Añadir una lista"); // al input le agrega atributos
+		var buttonAgregar = document.createElement("button");
+		var textButt = document.createTextNode("añadir lista");// crea texto para el boton
+		buttonAgregar.appendChild(textButt); //aqui agregamos el texto al boton
+	    formList.appendChild(buttonAgregar).classList.add("botonAgregar");
+	    formList.appendChild(listaInpu).classList.add("listaInput");
+	    var guardaContenedor = document.getElementById("guardaContenedor");
+	    formList.replaceChild(agregaLista);
+	 //    buttonAgregar.addEventListener("click",function(e){
+			
+		// 	var title=document.createElement("div");
+		// 	var textoTitle=inputTexto.value;
+		// 	title.innerHTML=textoTitle;
+		// 	formList.appendChild(title).classList.add("titleLista");
+			
+		// });
+
+		buttonAgregar.onclick = function guardarList(e){
+			var anterior = e.target.parentElement;
+			var nombreLista = document.createTextNode(listaInpu.value);
+			var contenedor = document.createElement("div");
+			contenedor.appendChild(nombreLista);
+			anterior.parentElement.replaceChild(contenedor, anterior);
+			contenedor.appendChild(listaInpu);
+			guardaContenedor.appendChild(contenedor);
+
+		}
+
+	});
 });
-
-
-
-agrega.addEventListener("click", function(){
-	var contenedor = document.createElement("div");
-	var listaInpu = document.createElement("input");
-	var buttonAgregar = document.createElement("button");
-	var textButt = document.createTextNode("añadir lista");
-	buttonAgregar.appendChild(textButt);
-    contenedor.appendChild(buttonAgregar);
-    contenedor.appendChild(listaInpu);
-});
-
