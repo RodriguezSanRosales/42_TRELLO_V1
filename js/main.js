1e8d7ea4d6bb64aa17ab4ac5659ece9a6c95b9fa
RodriguezSanRosales <san.rodzrosales@gmail.com>
2016-10-26 20:10:57 +0000

    agregando funciones en botones

--- js/main.js
+++ js/main.js
@@ -1,42 +1,53 @@
 window.addEventListener("load",function(){
 	var agregaLista = document.getElementById("agregaLista");
-	var formList = document.getElementById("formList");
-	
-	//agrega un focus con color de fondo
-	
+	// var formList = document.getElementById("formList");
+	var guardaContenedor = document.getElementById("guardaContenedor");
 
+
+	//agregando focus
+	// agregaLista.addEventListener("click",function(){
+	// 	agregaLista.style.backgroundColor = 'red';
+	// });
 
 	agregaLista.addEventListener("click",function(){
-		//var contenedor = document.createElement("div"); //crea un elemento div
+		var contenedor = document.createElement("div"); //crea un elemento div
 
 		var listaInpu = document.createElement("input"); // crea un elemento input
 		listaInpu.setAttribute("placeholder", "Añadir una lista"); // al input le agrega atributos
 		var buttonAgregar = document.createElement("button");
 		var textButt = document.createTextNode("añadir lista");// crea texto para el boton
 		buttonAgregar.appendChild(textButt); //aqui agregamos el texto al boton
-	    formList.appendChild(buttonAgregar).classList.add("botonAgregar");
-	    formList.appendChild(listaInpu).classList.add("listaInput");
-	    var guardaContenedor = document.getElementById("guardaContenedor");
-	    formList.replaceChild(agregaLista);
-	 //    buttonAgregar.addEventListener("click",function(e){
-			
-		// 	var title=document.createElement("div");
-		// 	var textoTitle=inputTexto.value;
-		// 	title.innerHTML=textoTitle;
-		// 	formList.appendChild(title).classList.add("titleLista");
-			
-		// });
-
+	    contenedor.appendChild(buttonAgregar).classList.add("buttonAgregar");
+	    contenedor.appendChild(listaInpu).classList.add("listaInpu");
+	    guardaContenedor.replaceChild(contenedor, agregaLista);
+	    
+	    //boton para guardar lista
 		buttonAgregar.onclick = function guardarList(e){
 			var anterior = e.target.parentElement;
 			var nombreLista = document.createTextNode(listaInpu.value);
-			var contenedor = document.createElement("div");
-			contenedor.appendChild(nombreLista);
+			var contenedorint = document.createElement("div");
+			var botonTarjeta = document.createElement("button");
+			var textTarjeta = document.createTextNode("añadir tarjeta");
+			var cajaTexto = document.createElement("textarea");
+			contenedorint.appendChild(cajaTexto);
+			botonTarjeta.appendChild(textTarjeta);
+			contenedorint.appendChild(botonTarjeta);
+			contenedorint.appendChild(nombreLista);
 			anterior.parentElement.replaceChild(contenedor, anterior);
-			contenedor.appendChild(listaInpu);
-			guardaContenedor.appendChild(contenedor);
+			contenedorint.appendChild(listaInpu);
+			guardaContenedor.appendChild(contenedorint);
 
+			//boton para guardar tarjeta
+
+			botonTarjeta.onclick = function guardarTarjeta(e){
+				var textoTarjeta = document.createTextNode(cajaTexto.value);
+				guardaContenedor.appendChild(textoTarjeta);
+
+			}
 		}
+
+
 
 	});
 });
+ 
